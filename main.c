827b46c18f593938d0853a2c2dbdb072506ce798
zhoexaeris <zhoe_gongon@dlsu.edu.ph>
2024-03-26 20:37:59 +0800

    modified C kernel (random X and Y values)
    added runtime for C

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <windows.h>
+#include <time.h>
 
-
+// C kernel function
 void daxpy_c(int n, double A, double* X, double* Y, double* Z) {
     for (int i = 0; i < n; ++i) {
         Z[i] = A * X[i] + Y[i];
@@ -10,27 +10,38 @@ void daxpy_c(int n, double A, double* X, double* Y, double* Z) {
 }
 
 int main() {
-    int n = 10;
+    int n = 2 << 20; // size of vector n (2^n)
     double A = 2.0;
-    double X[] = { 1.0, 2.0, 3.0 };
-    double Y[] = { 11.0, 12.0, 13.0 };
-    double* Z;
+    double* X = (double*)malloc(n * sizeof(double));
+    double* Y = (double*)malloc(n * sizeof(double));
+    double* Z = (double*)malloc(n * sizeof(double));
 
-    Z = (double*)malloc(n * sizeof(double));
-    if (Z == NULL) {
-        printf("Memory allocation failed.\n");
-        return 1;
+    // initialize X and Y for random values
+    srand(time(NULL));
+    for (int i = 0; i < n; ++i) {
+        X[i] = (double)rand() / RAND_MAX;
+        Y[i] = (double)rand() / RAND_MAX;
     }
 
+    // perform C kernel
+    clock_t start_time = clock(); // starting run time
     daxpy_c(n, A, X, Y, Z);
+    clock_t end_time = clock(); // ending run time
+    double total_time = (double)(end_time - start_time) / CLOCKS_PER_SEC; // calculate total time in seconds
 
+
+    // print first 10 elements of Z
     printf("Results for C version:\n");
     for (int i = 0; i < 10; ++i) {
         printf("%.2f \n", Z[i]);
     }
-    printf("\n");
 
-    free(Z); // Don't forget to free allocated memory
+    printf("C Execution time: %.9f seconds\n", total_time);
+
+    // free allocated memory
+    free(X);
+    free(Y);
+    free(Z);
 
     return 0;
 }
